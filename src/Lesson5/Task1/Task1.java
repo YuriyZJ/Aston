d084d22f3b12dfce1d6b0e930fc05f56db1cd961
YurıyZJ <yuriy.work93@gmail.com>
2025-02-27 22:02:15 +0300

    Lesson5 (Task1-3)

---
+++ src/Lesson5/Task1/Task1.java
@@ -0,0 +1,16 @@
+package Lesson5.Task1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(5, -3, 12, 8, -1, 10, -100);
+
+        numbers.stream()
+                .filter(n -> n >= 0) // оставляем неотрицательные
+                .map(n -> n * 2) // умножаем на 2
+                .forEach(System.out::println);
+    }
+}
---
+++ src/Lesson5/Task1/Task3.java
@@ -0,0 +1,16 @@
+package Lesson5.Task1;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Task3 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(3, 6, 2, 8, 5, 10, 7);
+
+        numbers.stream()
+                .filter(number -> number % 2 == 0) // Оставляем только четные числа
+                .map(number -> (int) Math.pow(number, 2)) // Возводим в квадрат
+                .sorted(Comparator.reverseOrder()) // Сортируем по убыванию
+                .forEach(number -> System.out.println(number)); // Выводим результат
+    }
+}
